d28ede5eab2025281fa2974459f17fdd9d4d22e3
kdhia <dkessabi@gmail.com>
2021-02-09 22:28:05 +0100

    cration de recuder add_reminder

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,12 @@
-const reducer = (state = [], action) => {
-  return state;
+import {ADD_REMINDER} from "../types"
+
+const reminders = (state = [], action) => {
+    let reminders = [];
+    if(action.type === ADD_REMINDER){
+      reminders = [...state , {text: action.text, date:action.date , id:Math.random()}]
+      console.log(reminders);
+      return reminders;
+    }
 };
 
-export default reducer;
+export default reminders;
